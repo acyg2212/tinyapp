9370d613be27e4eb306da1c780646997cf2d8089
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-04-08 14:30:32 -0400

    aadded mongo connection

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const db =
+const db = require("./config/keys").mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongo's a go"))
+    .catch(err => console.log(err))
 
 
 
-    app.get('/', (req, res) => res.send("This works!"));
+app.get('/', (req, res) => res.send("This works!"));
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is serving on port ${port}`))
