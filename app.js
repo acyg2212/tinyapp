5a4b916ae6c71a66a3fac321c7fc8c28197b2331
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-04-08 14:05:34 -0400

    built out server intiate database

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const db =
+
+
+
+    app.get('/', (req, res) => res.send("This works!"));
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is serving on port ${port}`))
